028c18f5a9bba9d8114cc80c7992a1c3abb25dfd
NaritaRadu <naritaradu0@gmail.com>
2026-08-29 18:43:52 +0300

    modified functional_tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
 import unittest
 from django.test import LiveServerTestCase
 
+MAX_WAIT = 5
 
 class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
@@ -13,11 +15,19 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
     
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, "tr")
-        self.assertIn(row_text, [row.text for row in rows])
-    
+    def wait_for_row_in_list_table(self, row_text):
+        start_time=time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, "id_list_table")
+                rows = table.find_elements(By.TAG_NAME, "tr")
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError,WebDriverException):
+                if time.time()-start_time>MAX_WAIT:
+                    raise
+                time.sleep(0.5)
+        
     def test_can_start_a_todo_list(self):
         # Stan has heard about a cool new online to-do app.
         # He goes to check out its homepage
@@ -34,8 +44,7 @@ class NewVisitorTest(LiveServerTestCase):
         # When he hits enter,the page updates, and now the page 
         # lists "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
         
        
         # There is still a text box inviting him to add another item
@@ -43,13 +52,44 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element(By.ID, "id_new_item")
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        
         
         # The page updates again, and now shows both items on her list
-        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
-        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.wait_for_row_in_list_table("2: Use peacock feathers to make a fly")
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
         
         #self.fail("Finish the test!")
     
-if __name__=='__main__':
-    unittest.main()
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        self.browser.get(self.live_server_url)
+        inputbox=self.browser.find_element(By.ID,"id_new_item")
+        inputbox.send_keys("Buy peacock feathers")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
+        
+        stan_list_url=self.browser.current_url
+        self.assertRegex(stan_list_url,"/lists/.+")
+        
+        self.browser.delete_all_cookies()
+        
+        self.browser.get(self.live_server_url)
+        page_text=self.browser.find_element(By.TAG_NAME,"body").text
+        self.assertNotIn("Buy peacock feathers",page_text)
+        
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox.send_keys("Buy milk")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Buy milk")
+
+    
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, "/lists/.+")
+        self.assertNotEqual(francis_list_url, stan_list_url)
+
+    
+        page_text = self.browser.find_element(By.TAG_NAME, "body").text
+        self.assertNotIn("Buy peacock feathers", page_text)
+        self.assertIn("Buy milk", page_text)
+        
+        
