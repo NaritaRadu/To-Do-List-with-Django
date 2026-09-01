f37c9da9d5a0d9236cdec288d7aadf0ea93ae51b
NaritaRadu <naritaradu0@gmail.com>
2026-09-01 23:24:12 +0300

    Add logging config to settings.py

--- src/superlists/settings.py
+++ src/superlists/settings.py
@@ -136,3 +136,13 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {"class": "logging.StreamHandler"},
+    },
+    "loggers": {
+        "root": {"handlers": ["console"], "level": "INFO"},
+    },
+}
