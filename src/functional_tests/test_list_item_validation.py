162b57fec2dd6ce9eb51c0fc2edeb62c2ee93b0c
NaritaRadu <naritaradu0@gmail.com>
2026-09-02 23:33:32 +0300

    return render(request, "list.html", {"list": our_list, "error": error})

--- src/functional_tests/test_list_item_validation.py
+++ src/functional_tests/test_list_item_validation.py
@@ -18,7 +18,7 @@ class ItemValidationTest(FunctionalTest):
         self.browser.find_element(By.ID,"id_new_item").send_keys("Purchase milk")
         self.browser.find_element(By.ID,"id_new_item").send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table("1: Purchase milk")
-        return
+        
         self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
         self.wait_for(
         lambda: self.assertEqual(
