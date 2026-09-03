8a3ff68cf0825724d021d9f1b5e90fc5385e21ee
NaritaRadu <naritaradu0@gmail.com>
2026-09-03 18:20:37 +0300

    use form in all views, back to working state

--- src/functional_tests/test_list_item_validation.py
+++ src/functional_tests/test_list_item_validation.py
@@ -7,25 +7,42 @@ from .base import FunctionalTest
 class ItemValidationTest(FunctionalTest):
     
     def test_cannot_add_empty_list_items(self):
+        # Edith goes to the home page and accidentally tries to submit
+        # an empty list item. She hits Enter on the empty input box
         self.browser.get(self.live_server_url)
-        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        # The browser intercepts the request, and does not load the list page
         self.wait_for(
-        lambda: self.assertEqual(  
-            self.browser.find_element(By.CSS_SELECTOR, ".invalid-feedback").text,
-            "You can't have an empty list item",
+            lambda: self.browser.find_element(By.CSS_SELECTOR, "#id_text:invalid")  
         )
-    )
-        self.browser.find_element(By.ID,"id_new_item").send_keys("Purchase milk")
-        self.browser.find_element(By.ID,"id_new_item").send_keys(Keys.ENTER)
+
+        # She starts typing some text for the new item and the error disappears
+        self.get_item_input_box().send_keys("Purchase milk")
+        self.wait_for(
+            lambda: self.browser.find_element(By.CSS_SELECTOR, "#id_text:valid")  
+        )
+
+        # And she can submit it successfully
+        self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table("1: Purchase milk")
-        
-        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
+
+        # Perversely, she now decides to submit a second blank list item
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        # Again, the browser will not comply
+        self.wait_for_row_in_list_table("1: Purchase milk")
         self.wait_for(
-        lambda: self.assertEqual(
-            self.browser.find_element(By.CSS_SELECTOR, ".invalid-feedback").text,
-            "You can't have an empty list item",
+            lambda: self.browser.find_element(By.CSS_SELECTOR, "#id_text:invalid")
         )
-    )
-        self.browser.find_element(By.ID, "id_new_item").send_keys("Make tea")
-        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
+
+        # And she can make it happy by filling some text in
+        self.get_item_input_box().send_keys("Make tea")
+        self.wait_for(
+            lambda: self.browser.find_element(
+                By.CSS_SELECTOR,
+                "#id_text:valid",
+            )
+        )
+        self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table("2: Make tea")
--- src/functional_tests/test_simple_list_creation.py
+++ src/functional_tests/test_simple_list_creation.py
@@ -14,7 +14,7 @@ class NewVisitorTest(FunctionalTest):
         header_text=self.browser.find_element(By.TAG_NAME,"h1").text
         self.assertIn("To-Do",header_text)
         # He is invited to enter a to-do item straight away
-        inputbox=self.browser.find_element(By.ID,"id_new_item")
+        inputbox=self.get_item_input_box()
         self.assertEqual(inputbox.get_attribute("placeholder"),"Enter a to-do item")
         inputbox.send_keys("Buy peacock feathers")
         # When he hits enter,the page updates, and now the page 
@@ -25,7 +25,7 @@ class NewVisitorTest(FunctionalTest):
        
         # There is still a text box inviting him to add another item
         # He enters " Use peacock feathers to make a fly"
-        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox = self.get_item_input_box()
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
         
@@ -38,7 +38,7 @@ class NewVisitorTest(FunctionalTest):
     
     def test_multiple_users_can_start_lists_at_different_urls(self):
         self.browser.get(self.live_server_url)
-        inputbox=self.browser.find_element(By.ID,"id_new_item")
+        inputbox=self.get_item_input_box()
         inputbox.send_keys("Buy peacock feathers")
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table("1: Buy peacock feathers")
@@ -52,7 +52,7 @@ class NewVisitorTest(FunctionalTest):
         page_text=self.browser.find_element(By.TAG_NAME,"body").text
         self.assertNotIn("Buy peacock feathers",page_text)
         
-        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox = self.get_item_input_box()
         inputbox.send_keys("Buy milk")
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table("1: Buy milk")
